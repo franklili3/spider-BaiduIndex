ce0a409e439ffc832c19a79b51c3b8b7bf352f78
franklili <348104201@qq.com>
2020-04-13 19:24:11 +0800

    print index

--- new_spider_without_selenium/demo.py
+++ new_spider_without_selenium/demo.py
@@ -2,14 +2,19 @@
 from get_index import BaiduIndex
 import pandas as pd
 
-if __name__ == "__main__":
-    keywords = ['比特币']
-    baidu_index = BaiduIndex(keywords, '2013-04-01', '2013-04-30')
-    baidu_index_all = pd.DataFrame(columns={'keyword', 'type', 'date', 'index'})
-    for index in baidu_index.get_index():
-        if index['type'] == 'all':
-            index_df = pd.DataFrame(index)
-            baidu_index_all.append(index_df)
+#if __name__ == "__main__":
+keywords = ['bitcoin']
+baidu_index = BaiduIndex(keywords, '2013-04-01', '2013-04-30')
+baidu_index_dict = {'keyword': [], 'type': [], 'date': [], 'index': []}
+for index in baidu_index.get_index():
+    print(index)
+'''     if index['type'] == 'all':
+        baidu_index_dict['keyword'].append(index['keyword'])
+        baidu_index_dict['type'].append(index['type'])
+        baidu_index_dict['date'].append(index['date'])
+        baidu_index_dict['index'].append(index['index'])
+ '''
+        
 
 
 # %%
