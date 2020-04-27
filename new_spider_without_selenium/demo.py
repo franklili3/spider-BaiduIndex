7c4bf3bc3550f2da3a3f92264f51740b4923378d
franklili <348104201@qq.com>
2020-04-27 21:14:15 +0800

    copy py to ipynb

--- new_spider_without_selenium/demo.py
+++ new_spider_without_selenium/demo.py
@@ -3,27 +3,15 @@ from get_index import BaiduIndex
 import pandas as pd
 
 #if __name__ == "__main__":
-keywords = ['bitcoin']
-baidu_index = BaiduIndex(keywords, '2013-04-01', '2013-04-30')
+keywords = ['比特币']
+baidu_index = BaiduIndex(keywords, '2013-04-01', '2014-04-30')
 baidu_index_dict = {'keyword': [], 'type': [], 'date': [], 'index': []}
 for index in baidu_index.get_index():
-    print(index)
-'''     if index['type'] == 'all':
+    #print(index)
+    if index['type'] == 'all':
         baidu_index_dict['keyword'].append(index['keyword'])
         baidu_index_dict['type'].append(index['type'])
         baidu_index_dict['date'].append(index['date'])
         baidu_index_dict['index'].append(index['index'])
- '''
-        
 
-
-# %%
-import requests
-
-url = 'http://www.baidu.com'
-
-resp = requests.get(url=url)
-print ('resp', resp.cookies._cookies)
-print ('req',  resp.request._cookies._cookies)
-
-# %%
+baidu_index_df = pd.DataFrame(baidu_index_dict)
